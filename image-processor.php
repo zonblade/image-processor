4d9b56009ae48229b5b3dbf243d872077a7ee142
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-08-28 00:31:55 +0700

    Create image-processor.php

---
+++ image-processor.php
@@ -0,0 +1,10 @@
+<?php
+namespace ImPro;
+/*
+Ini adalah plugin Image processing,
+harap install imagick sebelum menggunakan plugin ini
+plugin ini ditulis dan diolah oleh zonblade!
+*/
+
+require_once __DIR__.'/lib/ImageControlProcessor.php';
+require_once __DIR__.'/lib/ImagickProcessor.php';
