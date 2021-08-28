a58e0fd4079b9bf047707d884b3cbfdf387ff524
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-08-28 23:24:32 +0700

    adding try, catch to handle error message.

--- lib/ImageControlProcessor.php
+++ lib/ImageControlProcessor.php
@@ -8,7 +8,17 @@ class placer
 
     function __construct($working_dir)
     {
-        $this->dir = $working_dir;
+        try {
+            $this->dir = $working_dir;
+        } catch (\Throwable  $e) {
+            $getmsg = $e->getMessage();
+            return [
+                'status' => [
+                    'success' => false
+                ],
+                'reason' => $getmsg
+            ];
+        }
     }
 
     function post()
@@ -24,87 +34,96 @@ class placer
         $upload_limit       = func_get_args()[4]
         $replace_file       = func_get_args()[5]
         */
-        $allowed_extention = [];
-        $upload_limit      = 0;
-        if (isset(func_get_args()[3])) {
-            if (func_get_args()[3] !== false) {
-                $allowed_extention = func_get_args()[3];
+        try {
+            $allowed_extention = [];
+            $upload_limit      = 0;
+            if (isset(func_get_args()[3])) {
+                if (func_get_args()[3] !== false) {
+                    $allowed_extention = func_get_args()[3];
+                }
             }
-        }
-        if (isset(func_get_args()[4])) {
-            if (func_get_args()[4] !== false) {
-                $upload_limit      = (float)func_get_args()[4];
+            if (isset(func_get_args()[4])) {
+                if (func_get_args()[4] !== false) {
+                    $upload_limit      = (float)func_get_args()[4];
+                }
             }
-        }
-        $replace_file = true;
-        if (isset(func_get_args()[5])) {
-            if (func_get_args()[5] !== false) {
-                $replace_file = func_get_args()[5];
+            $replace_file = true;
+            if (isset(func_get_args()[5])) {
+                if (func_get_args()[5] !== false) {
+                    $replace_file = func_get_args()[5];
+                }
             }
-        }
 
-        $FILES = $_FILES['image_upload'];
-        if (isset(func_get_args()[0])) {
-            if (func_get_args()[0] !== false) {
-                $FILES = $_FILES[func_get_args()[0]];
-            }
-        }
-        /* mendefinisikan basis jika tidak ada args */
-        if (strpos($FILES['type'], 'image') !== false) {
-            $file_dir = $this->dir;
-            $filename = explode('.', $FILES['name']);
-            $filename = array_reverse($filename);
-            $file_ext = $filename[0];
-            $filename = str_replace('.' . $file_ext, '', $filename);
-            /* nama file */
-            $can_upload = true;
-            if (!empty($allowed_extention)) {
-                if (!in_array($file_ext, $allowed_extention)) {
-                    $can_upload = false;
+            $FILES = $_FILES['image_upload'];
+            if (isset(func_get_args()[0])) {
+                if (func_get_args()[0] !== false) {
+                    $FILES = $_FILES[func_get_args()[0]];
                 }
             }
-            if ($upload_limit !== 0) {
-                if ($FILES['size'] >= $upload_limit) {
-                    $can_upload = false;
-                }
-            }
-            if ($can_upload) {
-                if (isset(func_get_args()[1])) {
-                    if (func_get_args()[1] !== false) {
-                        $filename = func_get_args()[1];
+            /* mendefinisikan basis jika tidak ada args */
+            if (strpos($FILES['type'], 'image') !== false) {
+                $file_dir = $this->dir;
+                $filename = explode('.', $FILES['name']);
+                $filename = array_reverse($filename);
+                $file_ext = $filename[0];
+                $filename = str_replace('.' . $file_ext, '', $filename);
+                /* nama file */
+                $can_upload = true;
+                if (!empty($allowed_extention)) {
+                    if (!in_array($file_ext, $allowed_extention)) {
+                        $can_upload = false;
                     }
                 }
-                /* ekstensi file */
-                if (isset(func_get_args()[2])) {
-                    if (func_get_args()[2] !== false) {
-                        $file_ext = func_get_args()[2];
+                if ($upload_limit !== 0) {
+                    if ($FILES['size'] >= $upload_limit) {
+                        $can_upload = false;
                     }
                 }
-                /* menghilangkan double slash */
-                $file_path = $file_dir . '/' . $filename . '.' . $file_ext;
-                $file_path = str_replace('//', '/', $file_path);
-                if (file_exists($file_dir)) {
-                    if ($replace_file) {
-                        if (file_exists($file_path)) {
-                            unlink($file_path);
+                if ($can_upload) {
+                    if (isset(func_get_args()[1])) {
+                        if (func_get_args()[1] !== false) {
+                            $filename = func_get_args()[1];
                         }
-                        move_uploaded_file($FILES["tmp_name"], $file_path);
-                        return [
-                            'status' => [
-                                'success' => true
-                            ],
-                            'image' => [
-                                'name' => $filename . '.' . $file_ext,
-                                'path' => $file_path
-                            ],
-                            'reason' => 'Image successfully uploaded!'
-                        ];
+                    }
+                    /* ekstensi file */
+                    if (isset(func_get_args()[2])) {
+                        if (func_get_args()[2] !== false) {
+                            $file_ext = func_get_args()[2];
+                        }
+                    }
+                    /* menghilangkan double slash */
+                    $file_path = $file_dir . '/' . $filename . '.' . $file_ext;
+                    $file_path = str_replace('//', '/', $file_path);
+                    if (file_exists($file_dir)) {
+                        if ($replace_file) {
+                            if (file_exists($file_path)) {
+                                unlink($file_path);
+                            }
+                            move_uploaded_file($FILES["tmp_name"], $file_path);
+                            return [
+                                'status' => [
+                                    'success' => true
+                                ],
+                                'image' => [
+                                    'name' => $filename . '.' . $file_ext,
+                                    'path' => $file_path
+                                ],
+                                'reason' => 'Image successfully uploaded!'
+                            ];
+                        } else {
+                            return [
+                                'status' => [
+                                    'success' => false
+                                ],
+                                'reason' => 'Image not meant to be replaced!'
+                            ];
+                        }
                     } else {
                         return [
                             'status' => [
                                 'success' => false
                             ],
-                            'reason' => 'Image not meant to be replaced!'
+                            'reason' => 'Directory not found!'
                         ];
                     }
                 } else {
@@ -112,7 +131,7 @@ class placer
                         'status' => [
                             'success' => false
                         ],
-                        'reason' => 'Directory not found!'
+                        'reason' => 'Upload Denied!'
                     ];
                 }
             } else {
@@ -120,15 +139,16 @@ class placer
                     'status' => [
                         'success' => false
                     ],
-                    'reason' => 'Upload Denied!'
+                    'reason' => 'Not an Image!'
                 ];
             }
-        } else {
+        } catch (\Throwable  $e) {
+            $getmsg = $e->getMessage();
             return [
                 'status' => [
                     'success' => false
                 ],
-                'reason' => 'Not an Image!'
+                'reason' => $getmsg
             ];
         }
     }
@@ -142,78 +162,88 @@ class placer
         func_get_args()[3] = size limit, bisa di false kan
         func_get_args()[4] = apakan recrusive atau gakboleh replace?, bisa di false kan
         */
-        if (isset(func_get_args()[0])) {
-            $file_dir = $this->dir;
-            $base64 = func_get_args()[0];
-            $name   = 'image-uploaded-D_' . date('D-m-y') . '_' . date('H-i-s');
-            if (isset(func_get_args()[1])) {
-                if (func_get_args()[1] !== false) {
-                    $name   = func_get_args()[1];
+        try {
+            if (isset(func_get_args()[0])) {
+                $file_dir = $this->dir;
+                $base64 = func_get_args()[0];
+                $name   = 'image-uploaded-D_' . date('D-m-y') . '_' . date('H-i-s');
+                if (isset(func_get_args()[1])) {
+                    if (func_get_args()[1] !== false) {
+                        $name   = func_get_args()[1];
+                    }
                 }
-            }
-            $img_size_usr = 0;
-            if (isset(func_get_args()[3])) {
-                if (func_get_args()[3] !== false) {
-                    $img_size_usr   = func_get_args()[3];
+                $img_size_usr = 0;
+                if (isset(func_get_args()[3])) {
+                    if (func_get_args()[3] !== false) {
+                        $img_size_usr   = func_get_args()[3];
+                    }
                 }
-            }
-            $upload_n_replace = false;
-            if (isset(func_get_args()[4])) {
-                if (func_get_args()[4] !== false) {
-                    $upload_n_replace   = func_get_args()[4];
+                $upload_n_replace = false;
+                if (isset(func_get_args()[4])) {
+                    if (func_get_args()[4] !== false) {
+                        $upload_n_replace   = func_get_args()[4];
+                    }
                 }
-            }
-            $img_size = strlen(base64_decode($base64));
-            $can_upload = true;
-            if ($img_size_usr > 0) {
-                if ($img_size >= $img_size_usr) {
-                    $can_upload = false;
+                $img_size = strlen(base64_decode($base64));
+                $can_upload = true;
+                if ($img_size_usr > 0) {
+                    if ($img_size >= $img_size_usr) {
+                        $can_upload = false;
+                    }
                 }
-            }
-            $data = explode(',', $base64);
-            $data_info = $data[0];
-            $data_exts = strtok($data_info, ';');
-            $data_exts = substr($data_info, strpos($data_info, "/") + 1);
-            if (isset(func_get_args()[2])) {
-                if (func_get_args()[2] !== false) {
-                    $data_exts = func_get_args()[2];
+                $data = explode(',', $base64);
+                $data_info = $data[0];
+                $data_exts = strtok($data_info, ';');
+                $data_exts = substr($data_info, strpos($data_info, "/") + 1);
+                if (isset(func_get_args()[2])) {
+                    if (func_get_args()[2] !== false) {
+                        $data_exts = func_get_args()[2];
+                    }
                 }
-            }
-            $data_imgs = $data[1];
-            $file_path = $file_dir . $name . ".$data_exts";
-            if ($upload_n_replace) {
-                if (file_exists($file_path)) {
-                    $can_upload = false;
+                $data_imgs = $data[1];
+                $file_path = $file_dir . $name . ".$data_exts";
+                if ($upload_n_replace) {
+                    if (file_exists($file_path)) {
+                        $can_upload = false;
+                    }
                 }
-            }
-            if ($can_upload) {
-                $fp = fopen($file_path, "w+");
-                fwrite($fp, base64_decode($data_imgs));
-                fclose($fp);
-                return [
-                    'status' => [
-                        'success' => true
-                    ],
-                    'image' => [
-                        'name' => $name . ".$data_exts",
-                        'path' => $file_path
-                    ],
-                    'reason' => 'Image successfully uploaded!'
-                ];
+                if ($can_upload) {
+                    $fp = fopen($file_path, "w+");
+                    fwrite($fp, base64_decode($data_imgs));
+                    fclose($fp);
+                    return [
+                        'status' => [
+                            'success' => true
+                        ],
+                        'image' => [
+                            'name' => $name . ".$data_exts",
+                            'path' => $file_path
+                        ],
+                        'reason' => 'Image successfully uploaded!'
+                    ];
+                } else {
+                    return [
+                        'status' => [
+                            'success' => false
+                        ],
+                        'reason' => 'Upload Denied!'
+                    ];
+                }
             } else {
                 return [
                     'status' => [
                         'success' => false
                     ],
-                    'reason' => 'Upload Denied!'
+                    'reason' => 'No Base64 detected!'
                 ];
             }
-        } else {
+        } catch (\Throwable  $e) {
+            $getmsg = $e->getMessage();
             return [
                 'status' => [
                     'success' => false
                 ],
-                'reason' => 'No Base64 detected!'
+                'reason' => $getmsg
             ];
         }
     }
