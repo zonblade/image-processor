730367791d4afc7a34bcda5c261c6a3d0e2d86ee
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-08-29 19:46:50 +0700

    Create file-upload-form.php

---
+++ example/file-upload-form.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/image.php';
+
+if(isset($_POST['upload_gambar'])){
+    $image = new \ImPro\Image\placer(__DIR__.'/storage/');
+    $result = $image->post('upload_gambar');
+    if($result['status']['success']){
+        echo $result['reason']."\n";
+    }else{
+        echo $result['reason']."\n";
+    }
+}
+
+?>
+<form action="." method="POST" enctype="multipart/form-data">
+    <input type="file" name="upload_gambar" accept="image/*">
+    <input type="submit" value="submit">
+</form>
